1c01c118b686588b0221509533dabcc8c42cf4f2
MaximSemenyakin <70282170+MaximSemenyakin@users.noreply.github.com>
2020-11-06 21:29:27 +0300

    Add MVP Add DI Adapters

--- app/src/main/java/com/example/currencies/activities/MainActivity.kt
+++ app/src/main/java/com/example/currencies/activities/MainActivity.kt
@@ -1,23 +1,21 @@
-package com.example.currencies
+package com.example.currencies.activities
 
 import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import com.example.currencies.activities.AboutActivity
+import com.example.currencies.R
 import com.example.currencies.fragments.CurrinciesListFragment
-import kotlinx.android.synthetic.main.activity_about.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        bRateApp.setOnClickListener {
-            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=${applicationContext.packageName}")))
-        }
+//        bRateApp.setOnClickListener {
+//            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=${applicationContext.packageName}")))
+//        }
 
         if (savedInstanceState == null) {
             supportFragmentManager
